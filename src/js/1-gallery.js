886e367c6ea33f43e54c48d6e0602bfc4458356d
Anastasiia Savinova Occhiminuti <anastesie@hotmail.com>
2024-02-20 13:52:00 +0100

    commit finished

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,74 @@
-console.log("Gallery")
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { images } from "./gallery-items";
+
+const gallery = document.querySelector(".gallery-item");//hTML
+
+const pictures = images.map(({ preview, original, description }) => `<li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join("");//pour obtenir string on utilise join
+
+gallery.insertAdjacentHTML("beforeend", pictures);  
+
+const lightbox = new SimpleLightbox('.gallery-link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// lightbox.next();
+
+
+
+// const showImages  = JSON.parse(localStorage.getItem('images'));
+
+
+
+
+
+
+
+
+// const value = 100; RYSITCH
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sum(a, b) {
+//   return a * b + value;
+// }
+
+// // export { add, sum };
+
+// const a = 15;
+// const b = 20;
+
+// DEFAULT EXPORT
+// export { add, sum };
+// export default a;
+// console.log('hello');
+
+
+
+
+
+
+// На що буде звертати увагу ментор при перевірці:
+
+// На живій сторінці відображається галерея зображень із масиву даних images
+// Галерея зображень стилізована згідно з макетом
+// Дані для галереї створені динамічно в JS
+// Відсутні власні слухачі
+// Підключена бібліотека SimpleLightbox з використанням npm
+// Екземпляр бібліотеки ініціалізований після додавання елементів галереї у DOM і поза межами будь-яких функцій
+// При кліку по елементу галереї відкривається модальне вікно підключеної бібліотеки, в якому міститься збільшена версія зображення, по якому клікнули. Весь базовий функціонал бібліотеки працює
+// Через 250 мілісекунд після відкривання модального вікна вміст атрибута alt з’являється знизу, як підпис до зображення
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,60 @@
-console.log("form")
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener('input', saveInput);
+
+const showInput = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (showInput){
+   form.elements.email.value = showInput.email;
+    form.elements.message.value = showInput.message;
+}
+
+function saveInput() {
+    const formData = {
+        email: form.elements.email.value.trim(),
+        message: form.elements.message.value.trim()
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    
+}
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+    
+        if (form.elements.email.value.trim() === "" || form.elements.message.value.trim() === "") {
+            alert("All fields must be filled");
+            return;
+    }
+    const showSubmit = JSON.parse(localStorage.getItem('feedback-form-state'));
+    console.log(showSubmit);
+      
+    localStorage.removeItem('feedback-form-state')
+    form.reset()
+            
+})
+
+
+
+// rysitch Import from gallery
+
+// DEFAULT EXPORT is used really rarely. It has to be only ONE default file
+// import { galleryItems } from './gallery-items';
+
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// // import { sum as fnSum, add} from './02-video'; /// we changed the name with AS
+// // import * as data from './02-video'   /// * = all
+// // const sum = 10;
+// // console.log(galleryItems);
+// // console.log(sum);
+// // console.log(fnSum(4, 5));
+// // console.log(add(2,2));
+// // console.log(data.add(2,3));
+
+
+// import value from './02-video';
+// import {add,sum} from './02-video';
+// console.log(add(2,3));
+// console.log(sum(5,5));
+// console.log(value);
